f3166d1831310538ae1be6019a3712e044ab1f39
maxence_yrowah <kouame14@gmail.com>
2023-07-27 13:05:07 +0000

    RV-000 initi projet and add firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ricva-ui-app';
+  title = 'Hello world!';
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ricva-admin-managment',
+    appId: '1:808314028924:web:2de7c9905e8fb0d1875af9',
+    storageBucket: 'ricva-admin-managment.appspot.com',
+    apiKey: 'AIzaSyAdZZvp9lXgTFMhRY7qK2OSll-qc0ITKkc',
+    authDomain: 'ricva-admin-managment.firebaseapp.com',
+    messagingSenderId: '808314028924',
+  },
+};
