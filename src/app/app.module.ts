4ef72e2a752f97147b7c914a572ed935e65516ba
maxence_yrowah <kouame14@gmail.com>
2023-07-28 01:37:39 +0000

    RV-003 update login and register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,11 @@ import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +34,10 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
     provideFirestore(() => getFirestore()),
     BrowserAnimationsModule,
     FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  hidePassword: boolean = true;
+  formTitle: string = 'Connexion';
+  loginFormLogo: string = '../../../assets/img/logo.png';
+}
